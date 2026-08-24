c039dcb03d2111e3215abe72ba93705c0699fe04
noobient <Noobie8507@gmail.com>
2026-08-24 19:34:38 +0800

    Pin loader to corrected build

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(httpget("https://raw.githubusercontent.com/n0obient/noobient-autoplay/main/noobient_autoplay.lua"))()
+loadstring(httpget("https://raw.githubusercontent.com/n0obient/noobient-autoplay/c5d2966cdcdbe78ac7816a5a15abad842a1ca273/noobient_autoplay.lua"))()
